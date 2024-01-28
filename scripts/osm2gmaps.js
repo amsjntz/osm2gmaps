0a16fcc219e7c8405c39cb67179a43de94b2b49d
Amos <amsja.mull@gmail.com>
2024-01-28 14:19:58 +0100

    improved reliability

--- scripts/osm2gmaps.js
+++ scripts/osm2gmaps.js
@@ -1,6 +1,5 @@
 function create_link(coord_element) {
 	const spl = coord_element.href.split("/");
-	console.log(spl);
 
 	const link = document.createElement("a");
 	link.href = "https://www.google.com/maps/place/" + spl[4] + "," + spl[5] + "/";
@@ -15,9 +14,27 @@ function create_link(coord_element) {
 	return link;
 }
 
-var longitude_elements = document.getElementsByClassName('longitude');
-
-for (let element of longitude_elements) {
-	const coord_element = element.parentNode;
-	coord_element.parentNode.appendChild(create_link(coord_element));
+function has_link(element) {
+	const children = element.parentNode.parentNode.children;
+	for (let child of children) {
+		if (child.href === undefined) continue;
+		if (child.href.startsWith("https://www.google.com/maps")) {
+			return true;
+		}
+	}
+	return false;
 }
+
+function create_links() {
+	var longitude_elements = document.getElementsByClassName('longitude');
+
+	for (let element of longitude_elements) {
+		if (has_link(element)) {
+			continue;
+		}
+		const coord_element = element.parentNode;
+		coord_element.parentNode.appendChild(create_link(coord_element));
+	}
+}
+
+setInterval(create_links, 100);
